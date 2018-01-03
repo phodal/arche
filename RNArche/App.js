3db36ff980e4d046e45834e8d85b9a895c0b92da
Phodal HUANG <h@phodal.com>
2018-01-03 21:36:19 +0800

    [T] update bundle files

--- RNArche/App.js
+++ RNArche/App.js
@@ -4,8 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello, World!</Text>
+      <View>
+        <Text>
+          If you like React on the web, you'll like React Native.
+        </Text>
+        <Text>
+          You just use native components like 'View' and 'Text',
+          instead of web components like 'div' and 'span'.
+        </Text>
       </View>
     );
   }
