4391adb79993fc7c31f086cce295b7a984b8ecd8
Phodal HUANG <h@phodal.com>
2018-01-03 16:50:02 +0800

    [WebView] add loading

--- app/src/main/java/arche/phodal/com/arche/fragment/ArcheWebViewFragment.kt
+++ app/src/main/java/arche/phodal/com/arche/fragment/ArcheWebViewFragment.kt
@@ -9,23 +9,44 @@ import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
-
+import android.graphics.Bitmap
+import com.wang.avi.AVLoadingIndicatorView
 
 class ArcheWebViewFragment : Fragment() {
     private var mWebView: WebView? = null
+    private var avi: AVLoadingIndicatorView? = null
 
     @SuppressLint("SetJavaScriptEnabled")
     @Nullable
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater?.inflate(R.layout.fragment_webview, container, false)
+
+        avi = view?.findViewById(R.id.avi)
         mWebView = view?.findViewById(R.id.webview)
+
         mWebView!!.loadUrl("file:///android_asset/www/index.html")
 
         val webSettings = mWebView!!.settings
         webSettings.javaScriptEnabled = true
         mWebView!!.webViewClient = WebViewClient()
 
+        setLoadingProgress()
+
         return view
+    }
+
+    private fun setLoadingProgress() {
+        mWebView!!.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                avi!!.show()
+                super.onPageStarted(view, url, favicon)
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                avi!!.hide()
+                super.onPageFinished(view, url)
+            }
+        }
     }
 
 }
